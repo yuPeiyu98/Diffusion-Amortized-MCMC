9f8b39877cfe084ef6cde0c56f93fb2388763a11
Peiyu Yu <32430257+yuPeiyu98@users.noreply.github.com>
2023-01-26 15:00:55 -0800

    Update main_diffusion_shareQE.py

--- workspace/main_diffusion_shareQE.py
+++ workspace/main_diffusion_shareQE.py
@@ -160,11 +160,10 @@ def main(args):
         # g_loss = 1.0 / (2.0 * args.g_llhd_sigma * args.g_llhd_sigma) * torch.sum((x_hat - x) ** 2, dim=[1,2,3]).mean()
         g_loss = torch.sum((x_hat - x) ** 2, dim=[1,2,3]).mean()
 
-        if (iteration + 1) % 5 == 0:
-            g_loss.backward()
-            if args.g_is_grad_clamp:
-                torch.nn.utils.clip_grad_norm_(G.parameters(), max_norm=args.g_max_norm)
-            G_optimizer.step()
+        g_loss.backward()
+        if args.g_is_grad_clamp:
+            torch.nn.utils.clip_grad_norm_(G.parameters(), max_norm=args.g_max_norm)
+        G_optimizer.step()
 
         Q.eval()
         G.eval()
@@ -299,7 +298,7 @@ if __name__ == "__main__":
     # optimizing parameters
     parser.add_argument('--g_lr', type=float, default=5e-4, help='learning rate for generator')
     parser.add_argument('--e_lr', type=float, default=5e-5, help='learning rate for latent ebm')
-    parser.add_argument('--q_lr', type=float, default=5e-4, help='learning rate for inference model Q')
+    parser.add_argument('--q_lr', type=float, default=2e-4, help='learning rate for inference model Q')
     parser.add_argument('--q_is_grad_clamp', type=bool, default=True, help='whether doing the gradient clamp')
     parser.add_argument('--g_is_grad_clamp', type=bool, default=True, help='whether doing the gradient clamp')
     parser.add_argument('--q_max_norm', type=float, default=100, help='max norm allowed')
