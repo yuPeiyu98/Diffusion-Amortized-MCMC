d5ca966e99678d8f2cde2e4752a78b6f4f759d90
Peiyu Yu <32430257+yuPeiyu98@users.noreply.github.com>
2023-02-09 22:47:51 -0800

    Update dataset.py

--- workspace/data/dataset.py
+++ workspace/data/dataset.py
@@ -19,7 +19,7 @@ class CIFAR10Dataset(torchvision.datasets.CIFAR10):
     ):
         super(CIFAR10Dataset, self).__init__()
 
-    def __getitem__(index: int) → Tuple[Any, Any]:
+    def __getitem__(index: int) -> Tuple[Any, Any]:
         img, target = super().__getitem__(index)
 
         return img, index
