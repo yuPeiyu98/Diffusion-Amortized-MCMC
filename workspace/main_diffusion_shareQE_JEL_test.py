29c655088a28adcbcd9a829e3d965f67f449184f
Peiyu Yu <32430257+yuPeiyu98@users.noreply.github.com>
2023-04-08 11:08:39 -0700

    Update main_diffusion_shareQE_JEL_test.py

--- workspace/main_diffusion_shareQE_JEL_test.py
+++ workspace/main_diffusion_shareQE_JEL_test.py
@@ -18,8 +18,8 @@ import datetime as dt
 import re
 from data.dataset import MNIST
 from src.diffusion_net import _netG_cifar10, _netG_svhn, _netG_celeba64, _netE, _netQ, _netQ_uncond, _netQ_U
-from src.MCMC import sample_langevin_post_z_with_prior, sample_langevin_post_z_with_prior_mh, sample_langevin_post_z_with_gaussian
-from src.MCMC import gen_samples_with_diffusion_prior, calculate_fid_with_diffusion_prior, calculate_fid_with_diffusion_prior_E
+from src.MCMC import sample_langevin_post_z_with_prior, sample_langevin_post_z_with_gaussian
+from src.MCMC import gen_samples_with_diffusion_prior, calculate_fid_with_diffusion_prior, calculate_fid_with_diffusion_prior_E, calculate_fid
 
 
 torch.multiprocessing.set_sharing_strategy('file_system')
