a7be8a03826e8f70b80cbcff554bb8c232b4445a
Peiyu Yu <32430257+yuPeiyu98@users.noreply.github.com>
2023-04-25 00:45:52 -0700

    Update dataset.py

--- workspace/data/dataset.py
+++ workspace/data/dataset.py
@@ -190,28 +190,7 @@ class LSUN(VisionDataset):
 ########################################################################
 from torchvision.datasets import CIFAR10
 
-class CIFAR10(VisionDataset):
-    base_folder = "cifar-10-batches-py"
-    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
-    filename = "cifar-10-python.tar.gz"
-    tgz_md5 = "c58f30108f718f92721af3b95e74349a"
-    train_list = [
-        ["data_batch_1", "c99cafc152244af753f735de768cd75f"],
-        ["data_batch_2", "d4bba439e000b95fd0a9bffe97cbabec"],
-        ["data_batch_3", "54ebc095f3ab1f0389bbae665268c751"],
-        ["data_batch_4", "634d18415352ddfa80567beed471001a"],
-        ["data_batch_5", "482c414d41f54cd18b22e5b47cb7c3cb"],
-    ]
-
-    test_list = [
-        ["test_batch", "40351d587109b95175f43aff81a1287e"],
-    ]
-    meta = {
-        "filename": "batches.meta",
-        "key": "label_names",
-        "md5": "5ff9c542aee3614f3951f8cda6e48888",
-    }
-
+class CIFAR10(CIFAR10):
     def __init__(
         self,
         root: str,
@@ -220,42 +199,9 @@ class CIFAR10(VisionDataset):
         target_transform: Optional[Callable] = None,
         download: bool = False,
     ):
-        super().__init__(root, transform=transform, target_transform=target_transform)
-
-        self.train = train  # training set or test set
-
-        if self.train:
-            downloaded_list = self.train_list
-        else:
-            downloaded_list = self.test_list
-
-        self.data: Any = []
-        self.targets = []
-        # now load the picked numpy arrays
-        for file_name, checksum in downloaded_list:
-            file_path = os.path.join(self.root, self.base_folder, file_name)
-            with open(file_path, "rb") as f:
-                entry = pickle.load(f, encoding="latin1")
-                self.data.append(entry["data"])
-                if "labels" in entry:
-                    self.targets.extend(entry["labels"])
-                else:
-                    self.targets.extend(entry["fine_labels"])
-
-        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
-        self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
-
-        self._load_meta()
-
-    def _load_meta(self) -> None:
-        path = os.path.join(self.root, self.base_folder, self.meta["filename"])
-        with open(path, "rb") as infile:
-            data = pickle.load(infile, encoding="latin1")
-            self.classes = data[self.meta["key"]]
-        self.class_to_idx = {_class: i for i, _class in enumerate(self.classes)}
-
-    def __len__(self) -> int:
-        return len(self.data)
+        super().__init__(
+            root, train, transform, target_transform, download
+        )
     
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
         """
@@ -278,10 +224,6 @@ class CIFAR10(VisionDataset):
             target = self.target_transform(target)
 
         return img, index
-
-    def extra_repr(self) -> str:
-        split = "Train" if self.train is True else "Test"
-        return f"Split: {split}"
 
 ########################################################################
 ############################# MNIST ####################################
