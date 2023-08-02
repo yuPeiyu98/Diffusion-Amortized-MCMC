03a1fd8c2efc137da4e8949e18eb6a1513aa8e1e
Peiyu Yu <32430257+yuPeiyu98@users.noreply.github.com>
2023-08-01 19:28:29 -0700

    Create main_diffusion_shareQE_JEL_longrun_fid.py

---
+++ workspace/main_diffusion_shareQE_JEL_longrun_fid.py
@@ -0,0 +1,193 @@
+# Use both diffusion model (seperate models) as prior and posterior
+
+import argparse
+import numpy as np
+import os
+import os.path as osp
+import random
+import time
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision
+import torchvision.transforms as transforms
+import pytorch_fid_wrapper as pfw
+import shutil
+import datetime as dt
+import re
+from data.dataset import MNIST
+from src.diffusion_net import _netG_cifar10, _netG_svhn, _netG_celeba64, _netE, _netQ, _netQ_uncond, _netQ_U
+from src.MCMC import sample_langevin_post_z_with_prior, sample_langevin_post_z_with_gaussian
+from src.MCMC import gen_samples_with_diffusion_prior, calculate_fid_with_diffusion_prior, calculate_fid_with_diffusion_prior_E, calculate_fid
+
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+#################### training #####################################
+
+def main(args):
+
+    # initialize random seed
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+    
+    # make log directory  
+    timestamp = str(dt.datetime.now())[:19]
+    timestamp = re.sub(r'[\:-]','', timestamp) # replace unwanted chars 
+    timestamp = re.sub(r'[\s]','_', timestamp) # with regex and re.sub
+    
+    img_dir = os.path.join(args.resume_path, 'imgs')
+    ckpt_dir = os.path.join(args.resume_path, 'ckpt')
+    os.makedirs(img_dir, exist_ok=True)
+    os.makedirs(ckpt_dir, exist_ok=True)
+    shutil.copyfile(__file__, os.path.join(args.resume_path, osp.basename(__file__)))
+
+    # load dataset and calculate statistics
+    transform_train = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    if args.dataset == 'cifar10':
+        args.nz = 128
+        args.ngf = 128
+        trainset = torchvision.datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=transform_train)
+        testset = torchvision.datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=transform_test)
+        mset = torchvision.datasets.CIFAR10(root=args.data_path, train=False, download=True, transform=transform_test)
+    elif args.dataset == 'svhn':
+        args.nz = 100
+        args.ngf = 64
+        trainset = torchvision.datasets.SVHN(root=args.data_path, split='train', download=True, transform=transform_train)
+        testset = torchvision.datasets.SVHN(root=args.data_path, split='train', download=True, transform=transform_test) 
+        mset = torchvision.datasets.SVHN(root=args.data_path, split='test', download=True, transform=transform_test)
+    elif args.dataset == 'celeba64':
+        args.nz = 100
+        args.ngf = 128
+
+        transform_train = transforms.Compose([
+            transforms.Resize(64),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        transform_test = transforms.Compose([
+            transforms.Resize(64),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        trainset = torchvision.datasets.ImageFolder(root=osp.join(args.data_path, 'celeba64_train'), transform=transform_train)
+        testset = torchvision.datasets.ImageFolder(root=osp.join(args.data_path, 'celeba64_train'), transform=transform_test) 
+        mset = torchvision.datasets.ImageFolder(root=osp.join(args.data_path, 'celeba64_test'), transform=transform_test)
+    trainloader = data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=0, drop_last=True)
+    testloader = data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=0, drop_last=False)
+    mloader = data.DataLoader(mset, batch_size=500, shuffle=False, num_workers=0, drop_last=False)
+    train_iter = iter(trainloader)
+    
+    # pre-calculating statistics for fid calculation
+    start_time = time.time()
+    print("Begin calculating real image statistics")
+    fid_data_true = []
+    for x, _ in testloader:
+        fid_data_true.append(x)
+        if len(fid_data_true) >= args.n_fid_samples:
+            break
+    fid_data_true = torch.cat(fid_data_true, dim=0)
+    fid_data_true = (fid_data_true + 1.0) / 2.0
+    real_m, real_s = pfw.get_stats(fid_data_true, device="cuda:0")
+    print("Finish calculating real image statistics {:.3f}".format(time.time() - start_time), fid_data_true.shape, fid_data_true.min(), fid_data_true.max())
+    fid_data_true, testset, testloader = None, None, None
+
+    # define models
+    if args.dataset == 'cifar10':
+        G = _netG_cifar10(nz=args.nz, ngf=args.ngf, nc=args.nc)
+    elif args.dataset == 'svhn':
+        G = _netG_svhn(nz=args.nz, ngf=args.ngf, nc=args.nc)
+    elif args.dataset == 'celeba64':
+        G = _netG_celeba64(nz=args.nz, ngf=args.ngf, nc=args.nc)
+    Q = _netQ_U(nc=args.nc, nz=args.nz, nxemb=args.nxemb, ntemb=args.ntemb, nif=args.nif, \
+        diffusion_residual=args.diffusion_residual, n_interval=args.n_interval_posterior, 
+        logsnr_min=args.logsnr_min, logsnr_max=args.logsnr_max, var_type=args.var_type, with_noise=args.Q_with_noise, cond_w=args.cond_w,
+        net_arch='A', dataset=args.dataset)
+    Q_dummy = _netQ_U(nc=args.nc, nz=args.nz, nxemb=args.nxemb, ntemb=args.ntemb, nif=args.nif, \
+        diffusion_residual=args.diffusion_residual, n_interval=args.n_interval_posterior, 
+        logsnr_min=args.logsnr_min, logsnr_max=args.logsnr_max, var_type=args.var_type, with_noise=args.Q_with_noise, cond_w=args.cond_w,
+        net_arch='A', dataset=args.dataset)
+
+    E = _netE(nz=args.nz)
+
+    G.cuda()
+    Q.cuda()
+    E.cuda()
+    Q_dummy.cuda()
+
+    start_iter = 0
+    fid_best = 10000
+    mse_best = 10000
+    save_recon_imgs = False
+    if args.resume_path is not None:
+        ckpt_path = os.path.join(args.resume_path, 'ckpt/best.pth.tar')
+        print('load from ', ckpt_path)
+        state_dict = torch.load(ckpt_path)
+        G.load_state_dict(state_dict['G_state_dict'])
+        Q.load_state_dict(state_dict['Q_state_dict'])
+        Q_dummy.load_state_dict(state_dict['Q_dummy_state_dict'])
+        E.load_state_dict(state_dict['E_state_dict'])
+        
+        fid_s_time = time.time()
+        out_fid = calculate_fid(
+            n_samples=args.n_fid_samples, nz=args.nz, netG=G, netE=E,
+            e_l_steps=args.e_l_steps, e_l_step_size=args.e_l_step_size, e_l_with_noise=args.e_l_with_noise,
+            real_m=real_m, real_s=real_s, save_name='{}/fid_samples_{}.png'.format(img_dir, "test"))
+        print("Finish calculating fid time {:.3f} fid {:.3f} / {:.3f}".format(time.time() - fid_s_time, out_fid, fid_best))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=1, help='random seed')
+    parser.add_argument('--dataset', type=str, default='cifar10')
+    parser.add_argument('--log_path', type=str, default='../logs/cifar', help='log directory')
+    parser.add_argument('--data_path', type=str, default='../../noise_mixture_nce/ncebm_torch/data', help='data path')
+    parser.add_argument('--resume_path', type=str, default='../logs/cifar10/20230304_204909/', 
+                                         help='pretrained ckpt path for resuming training')
+    
+    # data related parameters
+    parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+    parser.add_argument('--nc', type=int, default=3, help='image channel')
+    parser.add_argument('--n_fid_samples', type=int, default=50000, help='number of samples for calculating fid during training')
+    
+    # network structure related parameters
+    parser.add_argument('--nz', type=int, default=128, help='z vector length')
+    parser.add_argument('--ngf', type=int, default=128, help='base channel numbers in G')
+    parser.add_argument('--nif', type=int, default=64, help='base channel numbers in Q encoder')
+    parser.add_argument('--nxemb', type=int, default=1024, help='x embedding dimension in Q')
+    parser.add_argument('--ntemb', type=int, default=128, help='t embedding dimension in Q')
+
+    # latent diffusion related parameters
+    parser.add_argument('--n_interval_posterior', type=int, default=100, help='number of diffusion steps used here')
+    parser.add_argument('--n_interval_prior', type=int, default=100, help='number of diffusion steps used here')
+    parser.add_argument('--logsnr_min', type=float, default=-5.1, help='minimum value of logsnr') # -5.1
+    parser.add_argument('--logsnr_max', type=float, default=9.8, help='maximum value of logsnr')  # 9.8
+    parser.add_argument('--diffusion_residual', type=bool, default=True, help='whether treat prediction as residual in latent diffusion model')
+    parser.add_argument('--var_type', type=str, default='large', help='variance type of latent diffusion')
+    parser.add_argument('--Q_with_noise', type=bool, default=True, help='whether include noise during inference')
+    parser.add_argument('--p_mask', type=float, default=0.2, help='probability of prior model')
+    parser.add_argument('--cond_w', type=float, default=0.0, help='weight of conditional guidance')
+    
+    # MCMC related parameters
+    parser.add_argument('--g_l_steps', type=int, default=30, help='number of langevin steps for posterior inference')
+    parser.add_argument('--g_l_step_size', type=float, default=0.1, help='stepsize of posterior langevin')
+    parser.add_argument('--g_l_with_noise', default=True, type=bool, help='noise term of posterior langevin')
+    parser.add_argument('--g_llhd_sigma', type=float, default=0.1, help='sigma for G loss')
+    parser.add_argument('--e_l_steps', type=int, default=100, help='number of langevin steps for prior sampling')
+    parser.add_argument('--e_l_step_size', type=float, default=0.4, help='stepsize of prior langevin')
+    parser.add_argument('--e_l_with_noise', default=True, type=bool, help='noise term of prior langevin')
+
+    args = parser.parse_args()
+    main(args)
