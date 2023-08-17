c8ade21ececc98e5a0790d5cef20e726bae1eb2b
Peiyu Yu <32430257+yuPeiyu98@users.noreply.github.com>
2023-08-17 14:56:16 -0700

    add deeper arch for G cifar10

--- workspace/main_diffusion_shareQE_JEL_deep.py
+++ workspace/main_diffusion_shareQE_JEL_deep.py
@@ -18,7 +18,7 @@ import datetime as dt
 import re
 from data.dataset import MNIST
 from src.diffusion_net import _netG_cifar10, _netG_svhn, _netG_celeba64, _netG_celebaHQ, _netE, _netQ, _netQ_uncond, _netQ_U
-from src.diffusion_net import Generator, _netG_cifar10_deep
+from src.diffusion_net import Generator, _netG_cifar10_deep, _netG_cifar10_deep_res
 from src.MCMC import sample_langevin_post_z_with_prior, sample_langevin_prior_z, sample_langevin_post_z_with_gaussian
 from src.MCMC import gen_samples_with_diffusion_prior, calculate_fid_with_diffusion_prior, calculate_fid
 
