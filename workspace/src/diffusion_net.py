5f22c5fe68cab1e960ad3eb5b766f7546b2dbadd
Peiyu Yu <32430257+yuPeiyu98@users.noreply.github.com>
2023-08-17 10:36:25 -0700

    add deep arch for CIFAR-10

--- workspace/src/diffusion_net.py
+++ workspace/src/diffusion_net.py
@@ -14,6 +14,65 @@ def spectral_norm(module, mode=True):
         return nn.utils.spectral_norm(module)
     return module
 
+########### Deep Generator ##############
+channels = 3
+
+GEN_SIZE=256
+
+class ResBlockGenerator(nn.Module):
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResBlockGenerator, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, 1, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, 1, padding=1)
+        self.bp = nn.Conv2d(in_channels, out_channels, 1, 1, padding=0)
+        nn.init.xavier_uniform_(self.conv1.weight.data, np.sqrt(2))
+        nn.init.xavier_uniform_(self.conv2.weight.data, np.sqrt(2))
+        nn.init.xavier_uniform_(self.bp.weight.data, 1.)
+
+        self.model = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2),
+            self.conv1,
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            self.conv2
+            )
+        self.bypass = nn.Sequential()
+        if stride != 1:
+            self.bypass =nn.Sequential(
+                    nn.Upsample(scale_factor=2),
+                    self.bp,
+                )
+
+    def forward(self, x):
+        return self.model(x) + self.bypass(x)
+
+class Generator(nn.Module):
+    def __init__(self, z_dim):
+        super(Generator, self).__init__()
+        self.z_dim = z_dim
+
+        self.dense = nn.Linear(self.z_dim, 4 * 4 * GEN_SIZE)
+        self.final = nn.Conv2d(GEN_SIZE, channels, 3, stride=1, padding=1)
+        nn.init.xavier_uniform_(self.dense.weight.data, 1.)
+        nn.init.xavier_uniform_(self.final.weight.data, 1.)
+
+        self.model = nn.Sequential(
+            ResBlockGenerator(GEN_SIZE, GEN_SIZE, stride=2),
+            ResBlockGenerator(GEN_SIZE, GEN_SIZE, stride=2),
+            ResBlockGenerator(GEN_SIZE, GEN_SIZE, stride=2),
+            nn.BatchNorm2d(GEN_SIZE),
+            nn.ReLU(),
+            self.final,
+            nn.Tanh())
+
+    def forward(self, z):
+        return self.model(self.dense(z).view(-1, GEN_SIZE, 4, 4))
+
+
 ########### Generator ###################
 class _netG_cifar10(nn.Module):
     def __init__(self, nz=128, ngf=128, nc=3, use_spc_norm=False):
